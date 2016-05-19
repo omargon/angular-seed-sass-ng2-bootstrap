9ace0b80274e6cab2a2f0edf044f146b28a8be2c
omargon <omargon@gmail.com>
2016-05-19 10:33:12 -0400

    Fix build.js.test.ts

--- tools/tasks/seed/build.js.test.ts
+++ tools/tasks/seed/build.js.test.ts
@@ -2,10 +2,16 @@ import * as gulp from 'gulp';
 import * as gulpLoadPlugins from 'gulp-load-plugins';
 import { join} from 'path';
 
-import { APP_DEST, APP_SRC, BOOTSTRAP_MODULE, TOOLS_DIR } from '../../config';
-import { templateLocals, makeTsProject } from '../../utils';
+import { APP_DEST, APP_SRC, BOOTSTRAP_MODULE, TMP_DIR, TOOLS_DIR } from '../../config';
+import { makeTsProject } from '../../utils';
 
 const plugins = <any>gulpLoadPlugins();
+
+const INLINE_OPTIONS = {
+  base: TMP_DIR,
+  useRelativePaths: false,
+  removeLineBreaks: true
+};
 
 export = () => {
   let tsProject = makeTsProject();
@@ -17,12 +23,12 @@ export = () => {
     '!' + join(APP_SRC, `${BOOTSTRAP_MODULE}.ts`)
   ];
   let result = gulp.src(src)
-  .pipe(plugins.plumber())
-  .pipe(plugins.sourcemaps.init())
-  .pipe(plugins.typescript(tsProject));
+    .pipe(plugins.plumber())
+    .pipe(plugins.sourcemaps.init())
+    .pipe(plugins.inlineNg2Template(INLINE_OPTIONS))
+    .pipe(plugins.typescript(tsProject));
 
-return result.js
-  .pipe(plugins.sourcemaps.write())
-  .pipe(plugins.template(templateLocals()))
-  .pipe(gulp.dest(APP_DEST));
+  return result.js
+    .pipe(plugins.sourcemaps.write())
+    .pipe(gulp.dest(APP_DEST));
 };
